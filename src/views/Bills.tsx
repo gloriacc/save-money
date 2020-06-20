90d2da580ae8fe502d74f00337624e6ed34621e9
gloriacc <xuan_1213@yeah.net>
2020-06-20 14:40:19 +0800

    完成记账页的数字输入基础功能

--- src/views/Bills.tsx
+++ src/views/Bills.tsx
@@ -17,27 +17,7 @@ const Bills = () => {
       <TagsSection/>
       <RemarkSection/>
       <CategorySection/>
-      <NumbersSection>
-        <div className="output">
-          100
-        </div>
-        <div className="keyboard">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">完成</button>
-          <button className="zero">0</button>
-          <button>.</button>
-        </div>
-      </NumbersSection>
+      <NumbersSection/>
     </BillsLayout>
   );
 }
--- src/views/Bills/NumbersSection.tsx
+++ src/views/Bills/NumbersSection.tsx
@@ -1,33 +1,54 @@
-import styled from 'styled-components';
+import React, {useState} from 'react';
+import {Wrapper} from './NumbersSection/Wrapper';
+import {calcOutput} from './NumbersSection/calcOutput';
 
-const NumbersSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  background: #FFF;
-  .output {
-    line-height: 72px;
-    text-align: right;
-    font-size: 36px;
-    padding: 0 16px;
-  }
-  .keyboard {
-    > button {
-      font-size: 18px;
-      text-align: center;
-      width: 25%;
-      height: 64px;
-      background: none;
-      outline: none;
-      float: left;
-      &.ok {
-        height: 128px;
-        float: right;
-      }
-      &.zero {
-        width: 50%;
-      }
+const NumbersSection: React.FC = () => {
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
+    }
+    _setOutput(output);
+  };
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as  HTMLButtonElement).textContent;
+    if (text === null) {
+      return;
+    }
+    if (text === '完成') {
+      // TODO
+      console.log('完成');
+      return;
+    }
+    if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text)) {
+      setOutput(calcOutput(text, output));
     }
   }
-`;
+  return (
+    <Wrapper>
+      <div className="output">
+        {output}
+      </div>
+      <div className="keyboard clearfix" onClick={onClickButtonWrapper}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">完成</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  )
+}
 
 export {NumbersSection};
